ef59e0923fb149e7f9342dd52afc2467db1f14c7
Julius F. Heins <jfheins@gmail.com>
2019-12-04 22:20:39 +0100

    Prepare day 5

---
+++ Day_05/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Core;
+using MoreLinq;
+
+namespace Day_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("../../../input.txt");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            _ = Console.ReadLine();
+        }
+    }
+}
