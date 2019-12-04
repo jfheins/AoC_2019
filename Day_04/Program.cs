bbb096835b3a7809af4e24e99f8be517312a2c80
Julius F. Heins <jfheins@gmail.com>
2019-12-04 12:17:59 +0100

    more performance optimizations

--- Day_04/Program.cs
+++ Day_04/Program.cs
@@ -12,24 +12,53 @@ namespace Day_04
         static void Main(string[] args)
         {
             var input = new string[] { "134564", "585159" };
-            var counts = new int[10000];
+            const int runs = 100000;
+            var counts = new int[runs];
 
             var sw = new Stopwatch();
             sw.Start();
 
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < runs; i++)
             {
-
                 var codes = NonDecreasingSequences(input[0].ToCharArray(), input[1].ToCharArray());
-                var codesWithDoubles = codes.Where(code => code.Chunks().Any(run => run.Count() == 2));
+                var codesWithDoubles = codes.Where(HasRunsof2orMore);
 
                 counts[i] = codesWithDoubles.Count();
             }
 
             Console.WriteLine($"Part 1: {counts[0]} codes fulfil this.");
             sw.Stop();
-            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds * 1000 / runs}µs on average.");
             _ = Console.ReadLine();
+        }
+
+        // For Part 1
+        static bool HasRunsof2orMore(char[] code)
+        {
+            var last = 'x';
+            foreach (var digit in code)
+            {
+                if (digit == last)
+                    return true;
+                last = digit;
+            }
+            return false;
+        }
+
+        // For Part 2
+        static bool HasRunsof2(char[] code)
+        {
+            var last = 'x';
+            var run = 0;
+            foreach (var digit in code)
+            {
+                if (digit != last && run == 2)
+                    return true;
+
+                run = digit == last ? run + 1 : 1;
+                last = digit;
+            }
+            return run == 2;
         }
 
         static IEnumerable<char[]> NonDecreasingSequences(char[] start, char[] end)
