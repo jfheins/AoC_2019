72321c9e0dd26dd207bb46bc9dbcf2de35188491
Julius F. Heins <jfheins@gmail.com>
2019-12-07 16:18:56 +0100

    Refactor looks good, tests still red

--- Core.Test/IntCodeComputerTests.cs
+++ Core.Test/IntCodeComputerTests.cs
@@ -92,6 +92,23 @@ namespace Core.Test
             Assert.AreEqual(37, computer.Outputs.First());
         }
 
+        [DataTestMethod]
+        [DataRow(6, 999)]
+        [DataRow(7, 999)]
+        [DataRow(8, 1000)]
+        [DataRow(9, 1001)]
+        [DataRow(10, 1001)]
+        [DataRow(11, 1001)]
+        public void InputOutputWorks2(int input, int expected)
+        {
+            var computer = new IntCodeComputer(new int[] { 3, 21, 1008, 21, 8, 20, 1005, 20, 22,
+                107, 8, 21, 20, 1006,20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20,
+                1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99 });
+            computer.Inputs.Add(input);
+            computer.Run();
+            Assert.AreEqual(expected, computer.Outputs.First());
+        }
+
         [TestMethod]
         public void ImmediateParamWorks()
         {
