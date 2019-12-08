273d2f408bb66996c536c84ccecd3e919a429a10
Julius F. Heins <jfheins@gmail.com>
2019-12-08 08:21:07 +0100

    Solve day 8

--- Day_08/Program.cs
+++ Day_08/Program.cs
@@ -11,9 +11,39 @@ namespace Day_08
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var input = File.ReadAllText("../../../input.txt").Select(chr => chr - '0').ToList();
+            const int imgWidth = 25;
+            const int imgHeight = 6;
+
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var layers = input.Chunks(imgWidth * imgHeight).Select(chunk => chunk.ToArray()).ToList();
+            var minlayer = layers.MinBy(l => l.Count(pixel => pixel == 0)).First().ToArray();
+            var checksum = minlayer.Count(p => p == 1) * minlayer.Count(p => p == 2);
+            Console.WriteLine($"Part 1: Pixel product = { checksum }");
+
+            var combined = layers
+                .Aggregate((a, b) => a.Zip(b).Select(tupel => tupel.First == 2 ? tupel.Second : tupel.First).ToArray());
+
+            Console.WriteLine();
+            for (int y = 0; y < imgHeight; y++)
+            {
+                for (int x = 0; x < imgWidth; x++)
+                {
+                    var chr = (combined[x + y * imgWidth]) switch { 0 => ' ', 1 => '█', 2 => 'o' };
+                    Console.Write(chr);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            _ = Console.ReadLine();
         }
     }
 }
