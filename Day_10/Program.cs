e335022b7956424d546cbb7b2be62ee803298190
Julius F. Heins <jfheins@gmail.com>
2019-12-09 21:57:19 +0100

    Prepare day 10

---
+++ Day_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Core;
+using Core.Combinatorics;
+using MoreLinq;
+
+namespace Day_10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("../../../input.txt");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Console.WriteLine($"Part 1: ");
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            _ = Console.ReadLine();
+        }
+    }
+}
