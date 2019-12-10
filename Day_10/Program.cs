3c8c19f6e7a4b9c7cb8e8f283c9fe169f3532794
Julius F. Heins <jfheins@gmail.com>
2019-12-10 08:13:03 +0100

    Solve part 2

--- Day_10/Program.cs
+++ Day_10/Program.cs
@@ -35,6 +35,31 @@ namespace Day_10
             var max = visibleAsteroids.MaxBy(x => x.Value).First();
             Console.WriteLine($"Part 1: {max.Key} sees {max.Value}");
 
+            var laserLocation = max.Key;
+            var targets = asteroids
+                .ExceptFor(laserLocation)
+                .Select(o => (o, ToPolar(laserLocation, o)))
+                .OrderBy(t => t.Item2.angle)
+                .ThenBy(t => t.Item2.distance)
+                .ToLookup(x => x.Item2.angle, new DoubleComparer());
+
+            var hits = new List<Point>();
+            for (int round = 0; round < 1000; round++)
+            {
+                var thisRound = targets
+                    .SelectMany(group => group
+                    .OrderBy(x => x.Item2.distance)
+                    .Skip(round)
+                    .Take(1))
+                    .ToList();
+                foreach (var target in thisRound)
+                {
+                    hits.Add(target.o);
+                    Console.WriteLine($"{hits.Count} {target.o} @ {target.Item2.angle}");
+                }
+            }
+
+            Console.WriteLine($"coords: {hits[199]}");
 
             sw.Stop();
             Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
@@ -44,6 +69,16 @@ namespace Day_10
         static double AngleBetween(Point a, Point b)
         {
             return Math.Atan2(b.Y - a.Y, b.X - a.X);
+        }
+
+        static (double distance, double angle) ToPolar(Point a, Point b)
+        {
+            var dx = b.X - a.X;
+            var dy = b.Y - a.Y;
+            var angle = Math.Atan2(-dy, dx);
+            angle = (Math.PI / 2) - angle;
+            angle = (angle + 2 * Math.PI) % (2 * Math.PI);
+            return (dx * dx + dy * dy, angle);
         }
     }
 
