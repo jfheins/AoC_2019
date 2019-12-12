10a1b6f59e107e5f5a267cb0c873cf58ae165c59
Julius F. Heins <jfheins@gmail.com>
2019-12-12 09:47:04 +0100

    wip part 2

--- Day_12/Program.cs
+++ Day_12/Program.cs
@@ -20,10 +20,25 @@ namespace Day_12
             sw.Start();
 
             var bodies = input.Select(s => new Body(Point3.FromArray(s.ParseInts(3)))).ToList();
+            var states = new List<int[]>();
 
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 100000; i++)
             {
                 Step(bodies);
+                foreach (var b in bodies)
+                {
+                    b.SaveState(i);
+                }
+                if (bodies.All(b => b.IsPeriodic))
+                {
+                    Console.WriteLine("All periodic");
+                    foreach (var b in bodies)
+                    {
+                        Console.WriteLine($"Period: {b.Period}");
+                    }
+
+                    break;
+                }
             }
 
             Console.WriteLine($"Part 1: {CalculateEnergy(bodies)}");
@@ -70,14 +85,53 @@ namespace Day_12
         public Point3 Position;
         public Vector3 Velocity = Vector3.Zero;
 
+        public List<Point3> PositionHistory = new List<Point3>();
+        public List<Vector3> VelHist = new List<Vector3>();
+
+        public HashSet<Point3> PastPos = new HashSet<Point3>();
+        public HashSet<Vector3> PastVel = new HashSet<Vector3>();
+
+        public int Period = 0;
+
+        public bool IsPeriodic => Period > 0;
+
         public Body(Point3 point3)
         {
             Position = point3;
+        }
+
+        public void SaveState(int iteration)
+        {
+            if (IsPeriodic)
+                return;
+
+            if (PastPos.Contains(Position))
+            {
+                if (PastPos.Contains(PositionHistory[PositionHistory.Count - 1])
+                    && PastPos.Contains(PositionHistory[PositionHistory.Count - 2]))
+                {
+
+                    // Possibly periodic
+                    var search = PositionHistory.GetRange(PositionHistory.Count - 10, 10);
+                    var first = PositionHistory.StartingIndex(search).First();
+                    if (first < PositionHistory.Count - 10)
+                    {
+                        Period = PositionHistory.Count - first - 10;
+                    }
+                }
+            }
+
+            PositionHistory.Add(Position);
+            PastPos.Add(Position);
+            //VelHist.Add(Velocity);
+            //PastVel.Add(Velocity);
         }
 
         public int Energy => (Math.Abs(Position.X) + Math.Abs(Position.Y) + Math.Abs(Position.Z))
             * (Math.Abs((int)Velocity.X) + Math.Abs((int)Velocity.Y) + Math.Abs((int)Velocity.Z));
 
         public override string ToString() => $"pos={Position}, vel={Velocity}";
+        public int[] ToArray()
+            => new int[] { Position.X, Position.Y, Position.Z, (int)Velocity.X, (int)Velocity.Y, (int)Velocity.Z };
     }
 }
