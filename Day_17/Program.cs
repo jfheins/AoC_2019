bf897b138c5406f9f322709a49c62aa065c73c71
Julius F. Heins <jfheins@gmail.com>
2019-12-17 09:34:53 +0100

    Solve day 17

--- Day_17/Program.cs
+++ Day_17/Program.cs
@@ -16,14 +16,44 @@ namespace Day_17
             sw.Start();
 
             //var input = File.ReadAllText("../../../input.txt").ParseLongs();
-            var computer = LongCodeComputer.FromFile(@"../../../input.txt");
+            var c = LongCodeComputer.FromFile(@"../../../input.txt");
+            c.Store(0, 2);
 
+            _ = c.RunUntilInputRequired();
+
+            Queue(c, "A,A,B,C,B,C,B,A,C,A\n");
+            Queue(c, "R,8,L,12,R,8\n");
+            Queue(c, "L,10,L,10,R,8\n");
+            Queue(c, "L,12,L,12,L,10,R,10\n");
+            Queue(c, "Y\n");
+            c.Outputs.Clear();
+
+            while (c.RunForOutputs(2200))
+            {
+                Paint(c.Outputs);
+                c.Outputs.Clear();
+            }
 
             Console.WriteLine($"Part 1: ");
 
             sw.Stop();
             Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
             _ = Console.ReadLine();
+        }
+
+        private static void Queue(LongCodeComputer c, string input)
+        {
+            foreach (var chr in input)
+            {
+                c.Inputs.Enqueue(chr);
+            }
+        }
+
+        private static void Paint(IEnumerable<long> laby)
+        {
+            Console.SetCursorPosition(0, 0);
+            var map = laby.Select(l => (char)l).ToArray();
+            Console.WriteLine(map);
         }
     }
 }
