aebd6a64b0c6054c37445b17108122fe1092af2c
Julius F. Heins <jfheins@gmail.com>
2019-12-19 08:32:37 +0100

    Solve part 2

--- Day_19/Program.cs
+++ Day_19/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using Core;
@@ -10,10 +11,17 @@ namespace Day_18
 {
     class Program
     {
+        private static long[] _input;
+        private static Dictionary<Point, bool> _pointCache;
+
         static void Main()
         {
             var sw = new Stopwatch();
             sw.Start();
+
+            _input = File.ReadAllText("../../../input.txt").ParseLongs();
+
+            _pointCache = new Dictionary<Point, bool>();
 
             var computer = LongCodeComputer.FromFile(@"../../../input.txt");
             var sum = 0;
@@ -21,22 +29,83 @@ namespace Day_18
             {
                 for (int x = 0; x < 50; x++)
                 {
-                    computer = LongCodeComputer.FromFile(@"../../../input.txt");
-                    computer.Inputs.Enqueue(x);
-                    computer.Inputs.Enqueue(y);
-                    var p = computer.RunForOutputs(1);
-                    var output = computer.Outputs.Dequeue();
-                    sum += (int)output;
-                    Console.Write(output == 1 ? '#' : '.');
+                    var inBeam = getPoint(new Point(x, y));
+                    sum += inBeam ? 1 : 0;
+                    Console.Write(inBeam ? '#' : '.');
                 }
                 Console.WriteLine();
             }
-
             Console.WriteLine($"Part 1: {sum}");
+
+            var probe = new Point(0, 10);
+
+            //var diagMove = new Size(1, 1);
+            var topLeft = Point.Empty;
+            while (topLeft == Point.Empty)
+            {
+                while (!getPoint(probe))
+                    probe = probe.MoveTo(Direction.Right);
+
+                var probe2 = probe;
+                while (getPoint(probe2))
+                    probe2 = probe2.MoveTo(Direction.Right);
+
+                var beamWidth = probe2.X - probe.X;
+
+                for (int x = 0; x < beamWidth - 90; x++)
+                {
+                    probe2 = probe + new Size(x, 0);
+                    var rect = new Rectangle(probe2, new Size(100, 100));
+                    if (allPointsInBeam(rect))
+                    {
+                        topLeft = rect.Location;
+                        //Paint(rect.Location, 100);
+                        break;
+                    }
+                }
+                probe = probe.MoveTo(Direction.Down);
+            }
+
+            Console.WriteLine($"Part 2: topleft point is at {topLeft} => answer = {topLeft.X * 10000 + topLeft.Y}");
 
             sw.Stop();
             Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
             _ = Console.ReadLine();
+        }
+
+        private static void Paint(Point start, int size)
+        {
+            for (int y = start.Y; y < start.Y + size; y++)
+            {
+                for (int x = start.X; x < start.X + size; x++)
+                {
+                    var inBeam = getPoint(new Point(x, y));
+                    Console.Write(inBeam ? '#' : '.');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool getPoint(Point p)
+        {
+            return _pointCache.GetOrAdd(p, pos =>
+            {
+                var computer = new LongCodeComputer(_input);
+                computer.Inputs.Enqueue(pos.X);
+                computer.Inputs.Enqueue(pos.Y);
+                var p = computer.RunForOutputs(1);
+                return computer.Outputs.Dequeue() == 1;
+            });
+        }
+
+        private static bool allPointsInBeam(Rectangle rect)
+        {
+            var res = true;
+            res = res && getPoint(rect.Location + new Size(rect.Width - 1, 0));
+            res = res && getPoint(rect.Location + new Size(0, rect.Height - 1));
+            res = res && getPoint(rect.Location + new Size(rect.Width - 1, rect.Height - 1));
+            res = res && getPoint(rect.Location);
+            return res;
         }
     }
 }
