0e6d95a54f6236d9477794f3b0ed354e1374e644
Julius Heins <jfheins@gmail.com>
2019-12-21 22:29:50 +0100

    Solve day 20 part 2

--- Day_20/Program.cs
+++ Day_20/Program.cs
@@ -13,12 +13,13 @@ namespace Day_20
     {
         private static FiniteGrid2D<char> _laby;
         private static Dictionary<Point, Point> _portals;
+        private static Rectangle _innerregion;
 
         static void Main()
         {
             var sw = new Stopwatch();
             sw.Start();
-            _laby = Grid2D.FromFile("../../../bsp1.txt");
+            _laby = Grid2D.FromFile("../../../input.txt");
 
             var labels = ReadLabels().ToList();
             Debug.Assert(labels.Count == 2 * labels.Select(x => x.name).Distinct().Count() - 2);
@@ -39,12 +40,33 @@ namespace Day_20
 
             var search = new DijkstraSearch<Point>(EqualityComparer<Point>.Default, Expander);
             var path = search.FindFirst(openLabels["AA"], p => p == openLabels["ZZ"]);
-
             Console.WriteLine($"Part 1: Path has {path.Length} steps.");
+
+
+            _innerregion = _laby.Bounds.InflatedCopy(-3, -3);
+            var search2 = new DijkstraSearch<(Point, int level)>(EqualityComparer<(Point, int)>.Default, Expander2);
+            var path2 = search2.FindFirst((openLabels["AA"], 0), p => p.Item1 == openLabels["ZZ"] && p.level == 0);
+            Console.WriteLine($"Part 2: Path has {path2.Length} steps.");
 
             sw.Stop();
             Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
             _ = Console.ReadLine();
+        }
+
+        private static IEnumerable<((Point, int level) node, float cost)> Expander2((Point p, int level) arg)
+        {
+            var position = arg.p;
+            foreach (var pos in _laby.Get4NeighborsOf(position).Where(n => _laby[n] == '.'))
+            {
+                yield return ((pos, arg.level), 1);
+            }
+            if (_portals.ContainsKey(position))
+            {
+                if (_innerregion.Contains(position))
+                    yield return ((_portals[position], arg.level + 1), 1);
+                else if (arg.level > 0)
+                    yield return ((_portals[position], arg.level - 1), 1);
+            }
         }
 
         private static IEnumerable<(Point node, float cost)> Expander(Point arg)
