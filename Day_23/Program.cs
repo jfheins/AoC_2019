e6948ffaedffbeac8b5c5560b41efc6238e2df35
Julius Heins <jfheins@gmail.com>
2019-12-22 23:34:33 +0100

    Prepare day 23

---
+++ Day_23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Core;
+using Core.Combinatorics;
+
+namespace Day_23
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            //var input = File.ReadAllText("../../../input.txt").ParseLongs();
+            var computer = LongCodeComputer.FromFile(@"../../../input.txt");
+
+
+            Console.WriteLine($"Part 1: ");
+
+            sw.Stop();
+            Console.WriteLine($"Solving took {sw.ElapsedMilliseconds}ms.");
+            _ = Console.ReadLine();
+        }
+    }
+}
